81edf776230fc486a9e90e6f40b8dc0de0b73a36
Eyal <eyalcohen308@gmail.com>
2019-05-11 11:45:53 +0300

    added files

---
+++ cmake-build-debug/CMakeFiles/ex4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4.dir/threadpool.c.o"
+  "CMakeFiles/ex4.dir/osqueue.c.o"
+  "CMakeFiles/ex4.dir/test.c.o"
+  "ex4.pdb"
+  "ex4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ threadpool.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "threadPool.h"
+#define  ERROR_PRINT "Error in system call\n"
+/**
+ * create thread pool with numOfThreads threads.
+ * @param numOfThreads number of threads in the pool.
+ * @return pointer to the tp object.
+ */
+ThreadPool *tpCreate(int numOfThreads) {
+    // first, allocate space for the tp.
+
+}
